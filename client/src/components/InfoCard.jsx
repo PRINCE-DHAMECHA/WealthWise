194cada335a791c677b637d52dcf8218fe0eb328
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2023-05-25 00:16:29 +0530

    InfoData added

--- client/src/components/InfoCard.jsx
+++ client/src/components/InfoCard.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAppContext } from "../context/appContext";
 import { MdNavigateNext } from "react-icons/md";
 import { useLocation } from "react-router-dom";
+import {InformationData} from "../data/dummy";
 
 const InfoCard = ({}) => {
   const { currentColor } = useAppContext();
+  const [details, setDetails] = useState('');
+  const [title,setTitle] = useState('');
 
   let r = parseInt(currentColor.substring(1, 3), 16);
   let g = parseInt(currentColor.substring(3, 5), 16);
@@ -17,10 +20,22 @@ const InfoCard = ({}) => {
   const location = useLocation();
   const currentRoute = location.pathname.replace("/", "");
 
+  const handleButtonClick = () => {
+    const infoFetched = InformationData.find((obj) => obj.routeName === currentRoute);
+    const rand = Math.floor(Math.random() * 15);
+    const newDetails = infoFetched.contents[rand];
+    setDetails(newDetails);
+    setTitle(infoFetched.title);
+  };
+
+  useEffect(() => {
+    handleButtonClick();
+  }, []);
+
   return (
     <div
       style={{ zIndex: "100000" }}
-      className="md:w-1/4 w-4/5 h-1/4 fixed bottom-10 right-5"
+      className="md:w-1/4 w-4/5 h-1/4 fixed bottom-10 right-5 overflow-scroll scrollClass"
     >
       <div
         style={{
@@ -33,16 +48,15 @@ const InfoCard = ({}) => {
         className="rounded-lg relative"
       >
         <h2 className="text-xl text-white font-bold px-4 py-2">
-          {currentRoute}
+          {title}
         </h2>
         <p className="text-themeColor text-white px-4 py-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ipsam
-          error asperiores dolorem iste quasi eaque esse possimus delectus quae
-          ad culpa dolores ducimus consequatur, quia debitis molestiae animi
-          praesentium.
+          {details}
         </p>
         <div className="flex justify-end">
-          <button className="absolute top-2 right-6 text-xl text-white">
+          <button 
+          className="absolute top-2 right-6 text-xl text-white"
+          onClick = {handleButtonClick}>
             <MdNavigateNext />
           </button>
         </div>
